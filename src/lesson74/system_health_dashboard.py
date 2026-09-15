ab2a2b684fe719591b3c8a4710a144678e4b0e6e
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-09-15 21:51:08 +0900

    feature(top_cpu): retrieve cpu percent and sort as list

--- src/lesson74/system_health_dashboard.py
+++ src/lesson74/system_health_dashboard.py
@@ -2,6 +2,8 @@ import psutil
 
 
 def check_disk_health(threshold_percent: float = 85.0) -> dict:
+    '''Inspect the partition mounted at root and return the usage percentage.
+    If the usage parcentage exceeds the given threshold, it returns is_unhealthy True'''
     # Returns volume stats and an alert flag if usage exceeds threshold.
 
     # Capture root disk metrics
@@ -15,16 +17,32 @@ def check_disk_health(threshold_percent: float = 85.0) -> dict:
 
     # Return 
     return {'disk_used_percent' : used_disk_max_percent,
-            'is_unhealth' : used_disk_max_percent > threshold_percent
-        }
+            'is_unhealthy' : used_disk_max_percent > threshold_percent}
 
 def check_memory_disk(threshold_percent: float = 80.0) -> dict:
     # Returns RAM stats and an alert flag if usage exceeds threshold.
-    return {}
+
+    cpu_usage = psutil.cpu_percent(interval= 1)
+    memory_info = psutil.virtual_memory()
+
+    return {"cpu_load_percent": cpu_usage,
+            "ram_totla_gb": round(memory_info.total / (1024**3), 2),
+            "ram_used_percent": memory_info.percent,
+            "is_unhealthty": cpu_usage > threshold_percent}
 
 def get_top_cpu_process(count: int = 5) -> list:
     # Returns the top CPU consumers.
-    return []
+
+    try:
+        proc = psutil.process_iter(['pid', 'name', 'cpu_percent'])
+        sorted_pros = sorted(proc, key= lambda proc: proc.info['cpu_percent'], reverse= True)
+        top_cpu = []
+        for p in sorted_pros[:count]:
+            top_cpu.append(p.info['name'])
+    except (psutil.NoSuchProcess, psutil.AccessDenied) as e:
+        print(e)
+
+    return top_cpu
 
 def generate_health_report() -> dict:
     # Combines all checks into a single structured summary dictionary.
@@ -34,3 +52,9 @@ def generate_health_report() -> dict:
 volume_stats = check_disk_health()
 for key in volume_stats:
     print(f'{key}, {volume_stats[key]}')
+
+memory_stats = check_memory_disk()
+for key in memory_stats:
+    print(f'{key}: {memory_stats[key]}')
+
+print(get_top_cpu_process())
